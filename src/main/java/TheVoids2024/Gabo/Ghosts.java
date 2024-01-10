30f1157bf71958870dfb112a0ca6b6bc9c9ed473
Gabriel-Tam <gabrieltamayo.02.27@gmail.com>
2024-01-09 18:22:01 -0600

    pruebas con fantasmas

--- src/main/java/TheVoids2024/Gabo/Ghosts.java
+++ src/main/java/TheVoids2024/Gabo/Ghosts.java
@@ -1,5 +1,20 @@
 package TheVoids2024.Gabo;
 
-public class Ghosts {
+import java.util.EnumMap;
+
+import pacman.controllers.GhostController;
+import pacman.game.Constants.DM;
+import pacman.game.Constants.GHOST;
+import pacman.game.Constants.MOVE;
+import pacman.game.Game;
+
+public class Ghosts extends GhostController
+{
+
+    @Override
+    public EnumMap<GHOST, MOVE> getMove(Game game, long timeDue) {
+        // TODO Auto-generated method stub
+        throw new UnsupportedOperationException("Unimplemented method 'getMove'");
+    }	
     
 }
